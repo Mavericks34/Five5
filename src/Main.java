ff00dd6053fb3a7bf3b23b526818df4315b30536
origin <httpsgit@github.com:Mavericks34/helloworldd.git>
2026-04-08 18:20:47 +0300

    Bugs fixed

--- src/Main.java
+++ src/Main.java
@@ -3,52 +3,52 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        byte clientOS = 1;
+        byte clientOS = 0;
         if (clientOS == 0) {
             System.out.println("Установите приложение для IOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке ");
+        } else {
+            System.out.println("Ошибка ввода");
         }
         System.out.println("Задание 2");
-        short clientDeviceYear = 2010;
+        short clientDeviceYear = 2015;
         if (clientOS == 0 && clientDeviceYear <= 2015) {
             System.out.println("Установите облегчённую версию для IOS по ссылке ...");
+        } else if (clientOS > 1 && clientDeviceYear < 2015) {
+            System.out.println("Не корректный ввод операционной системы");
         } else if (clientOS == 1 && clientDeviceYear <= 2015) {
             System.out.println("Установите облегчённую версию приложения для Android по ссылке ...");
         } else if (clientDeviceYear > 2015 && clientOS == 0) {
             System.out.println("Установите приложение для IOS по ссылке");
         } else if (clientDeviceYear > 2015 && clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
+            System.out.println("Установите версию приложениядля Android по ссылке");
+        } else if (clientOS > 1 && clientDeviceYear > 2015) {
+            System.out.println("Не корректный ввод операционной системы");
+        } else {
+            System.out.println("Неизвестная ошибка");
         }
+
+
         System.out.println("Задание 3");
         short year = 2021;
-        if (year > 1584) {
-            if (year % 4 == 0) {
-                if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        System.out.println(year + " високосный");
-                    } else {
-                        System.out.println(year + " не високосный");
-                    }
-                } else {
-                    System.out.println(year + " високосный");
-                }
-            } else {
-                System.out.println(year + " не високосный.");
-            }
+        if (year >= 1584 && (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " високосный год");
+        } else if (year < 1584) {
+            System.out.println(year + " год , теоретически можно определить является ли он високосным или нет , но начало исчисления високосного года началось с 1584 года");
         } else {
-            System.out.println("Год должен быть больше 1584");
-        }
+            System.out.println(year + " не високосный");
+                    }
 
 
         System.out.println("Задание 4");
-        long deliveryDistance = 95;
+        long deliveryDistance = 95L;
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней 1");
+            System.out.println("Потребуется 1 день");
         } else if (deliveryDistance >= 20 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней 2");
+            System.out.println("Потребуется 2 дня");
         } else if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
+            System.out.println("Доставка свыше 100 км невозможна.");
         }
 
         System.out.println("Задание 5");
@@ -60,29 +60,26 @@ public class Main {
             case 12:
             case 1:
             case 2:
-                System.out.println("Зимний");
+                System.out.println(monthNumber + " месяц является зимним");
                 break;
             case 3:
             case 4:
             case 5:
-                System.out.println("Весенний");
+                System.out.println(monthNumber + " месяц является весенним");
                 break;
             case 6:
             case 7:
             case 8:
-                System.out.println("Летний");
+                System.out.println(monthNumber + " месяц является летним");
                 break;
             case 9:
             case 10:
             case 11:
-                System.out.println("Осенний");
+                System.out.println(monthNumber + " месяц является осенним");
                 break;
         }
-
-
-
-
-
-        System.out.println("Hard");
     }
 }
+
+
+
